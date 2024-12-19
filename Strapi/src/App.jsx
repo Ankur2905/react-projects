e0f1c4e3aae19c2763cdccf3c7e6b4895b0a259a
Ankur <singhankur362@gmail.com>
2024-12-20 01:43:41 +0530

    Initial setup

---
+++ Strapi/src/App.jsx
@@ -0,0 +1,20 @@
+import Hero from "./Hero"
+import Navbar from "./Navbar"
+import SideBar from "./SideBar"
+import Submenu from "./Submenu"
+
+
+
+function App() {
+
+  return (
+    <main>
+      <Navbar/>
+      <Hero/>
+      <SideBar/>
+      <Submenu/>
+    </main>
+  )
+}
+
+export default App
