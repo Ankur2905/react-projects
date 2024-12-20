f5cfad0436eadede596fe897f7d7249528a8322e
Ankur <singhankur362@gmail.com>
2024-12-21 02:07:29 +0530

    Added Sidebar and Hero section

--- Strapi/src/Hero.jsx
+++ Strapi/src/Hero.jsx
@@ -1,6 +1,13 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>Manage Any Content <br/> Anywhere</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, autem rerum in est sint accusantium iste dolor hic qui molestiae.
+        </p>
+      </div>
+    </div>
   )
 }
 export default Hero
