4d33afef8d6f29f71f28ae583a6ae1f856f713be
Ankur <singhankur362@gmail.com>
2024-12-25 23:46:27 +0530

    Added basic project structure

---
+++ mixmaster/src/App.jsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  About,
+  HomeLayout,
+  Landing,
+  Error,
+  Newsletter,
+  Cocktail,
+} from "./pages";
+
+import { loader as landingLoader } from "./pages/Landing";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        loader: landingLoader,
+      },
+      {
+        path: "cocktail",
+        element: <Cocktail />,
+      },
+      {
+        path: "newsletter",
+        element: <Newsletter />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
